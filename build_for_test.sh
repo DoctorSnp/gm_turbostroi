a5b1ca1f1af26e8bbadffb5163db32560fe9605c
DoctorSnp <den2011chs8@gmail.com>
2020-01-06 19:38:22 +0000

    rename build script and hide some garbage

---
+++ build_for_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+TARGET="$HOME/MetrostroiServer/steam_server/garrysmod/lua/bin"
+NAME="gmsv_turbostroi_linux.so"
+NAME_INSTALL="gmsv_turbostroi_linux.dll"
+
+rm -rf build 2> /dev/null
+mkdir build
+cd build &&
+cmake .. &&
+cmake --build . -- -j9 &&
+
+rm -f "$TARGET/$NAME_INSTALL"
+cp "$NAME" "$TARGET/$NAME_INSTALL"
---
+++ include/stdafx.h
@@ -0,0 +1,8 @@
+/*Это просто заглушка для сборки зависимостей т.к. они требуют этот файл*/
+
+#ifndef __TURBOSTROI_PSEUDO_STDAFX_H__
+#define __TURBOSTROI_PSEUDO_STDAFX_H__ 
+
+#undef _DEBUG
+
+#endif //__TURBOSTROI_PSEUDO_STDAFX_H__ 
